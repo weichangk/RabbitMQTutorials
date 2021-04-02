23e871dd77877dfda33b8c21d4a9c28e45ddf66c
unknown <weichangk>
2021-04-02 17:56:36 +0800

    add RPC

---
+++ RabbitMQDemo/RPC/RPCServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RPCServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("rpc_queue", false, false, false, null);
+                    channel.BasicQos(0, 1, false);
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume("rpc_queue", false, consumer);
+                    Console.WriteLine(" [x] Awaiting RPC requests");
+
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        string response = string.Empty;
+                        var body = ea.Body;
+                        var props = ea.BasicProperties;
+                        var replyProps = channel.CreateBasicProperties();
+                        replyProps.CorrelationId = props.CorrelationId;
+
+                        try
+                        {
+                            var message = Encoding.UTF8.GetString(body.ToArray());
+                            int n = int.Parse(message);
+                            Console.WriteLine(" [.] fib({0})", message);
+                            response = fib(n).ToString();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(" [.] " + e.Message);
+                            response = "";
+                        }
+                        finally
+                        {
+                            var responseBytes = Encoding.UTF8.GetBytes(response);
+                            channel.BasicPublish("", props.ReplyTo, replyProps, responseBytes);
+                            channel.BasicAck(ea.DeliveryTag, false);
+                        }
+                    };
+
+                }
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1) return n;
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+}
---
+++ RabbitMQDemo/RPC/RpcClient/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace RpcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RPCClient();
+
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+            Console.WriteLine(" [.] Got '{0}'", response);
+
+            rpcClient.Close();
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+
+    class RPCClient
+    {
+        private IConnection connection;
+        private IModel channel;
+        private string replyQueueName;
+        private EventingBasicConsumer consumer;
+
+        public RPCClient()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare();
+            consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(replyQueueName, true, consumer);
+        }
+
+        public string Call(string message)
+        {
+            string response = string.Empty;
+            var corrId = Guid.NewGuid().ToString();
+            var props = channel.CreateBasicProperties();
+            props.ReplyTo = replyQueueName;
+            props.CorrelationId = corrId;
+
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("", "rpc_queue", props, messageBytes);
+
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                if (ea.BasicProperties.CorrelationId == corrId)
+                {
+                    response = Encoding.UTF8.GetString(ea.Body.ToArray());
+                }
+            };
+            return response;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+
+
+}
